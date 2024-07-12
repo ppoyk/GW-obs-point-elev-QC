d002555bd7594d18d5b7a96dd7c4c273481e0972
ppoyk <138587496+ppoyk@users.noreply.github.com>
2024-07-12 10:03:07 +0300

    draft a function to check man vs AM differences

---
+++ R/funktiot/draft!_compare_man_AM_metadata_diffs.R
@@ -0,0 +1,24 @@
+# An example of a function body to check discrepancies in metadata between
+# linked manual and automatic GW monitoring places 
+
+# Not universal, meaning that appropriate variables and data must be available
+# beforehand.
+# Could be developed into a self-contained function.
+
+koord_vrt <- apply(link_am_man, 1, function(x) {
+  
+  man <- kaikki_paikat[kaikki_paikat$paikka_id %in% x[[2]], c("lon", "lat")]
+  am  <- kaikki_paikat[kaikki_paikat$paikka_id %in% x[[1]], c("lon", "lat")]
+  
+  if (all(!is.na(kaikki_paikat[kaikki_paikat$paikka_id %in% x[[2]], c("yhd_lon", "yhd_lat")])) &&
+      all(!is.na(kaikki_paikat[kaikki_paikat$paikka_id %in% x[[1]], c("yhd_lon", "yhd_lat")]))) {
+    if (isTRUE(all.equal(as.num(man), as.num(am)))) {
+      return(NULL) 
+    } else {
+      warning(paste("Db koord eroavat:", x[[1]],x[[2]],"\n"))
+    }
+  } else {
+    return(NULL) # Yhd koordit löytyvät, ei haittaa vaikka db:ssä erit
+  }
+}
+)
