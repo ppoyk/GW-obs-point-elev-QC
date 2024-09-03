f2f9f764a17fbd96e01e5b04b16e949e30971737
ppoyk <138587496+ppoyk@users.noreply.github.com>
2024-09-03 15:21:06 +0300

    add last fixes, add comments + ws

--- R/outkorkeus_investigated_fixes.R
+++ R/outkorkeus_investigated_fixes.R
@@ -81,39 +81,41 @@ putki_yhd_temp[putki_yhd_temp$paikka_id == 9935, "yla"] <-
 set_maa_w_putki(9935); copy_sources(9935, from = 43689)
 
 # Karkkila 0102 p8 ja p8b tietokannasta, parilla parempi yhteneväisyys.
-set_yla_from_db(50438); set_maa_w_putki(50438)
-set_yla_from_db(10148); set_maa_w_putki(10148)
+set_yla_from_db(10148); set_maa_w_putki(10148) # p8
+set_yla_from_db(50438); set_maa_w_putki(50438) # p8b
 
 # Perniö 0202 p10 ja p10AM yläpää tietokannasta. Maanpinta putken mukaan.
-set_yla_from_db(9730); set_maa_w_putki(9730)
-set_yla_w_id(100207, from = 9730); set_maa_w_id(100207, from = 9730)
+set_yla_from_db(9730); set_maa_w_putki(9730) # p10
+set_yla_w_id(100207, from = 9730); set_maa_w_id(100207, from = 9730) # p10AM
 
 # Oripää 0203 p3 ja p26 tietokannasta (DTM tukee hyvin, ELY hieman pielessä)
-set_yla_from_db(9613); set_maa_w_putki(9613)
-set_yla_from_db(9615); set_maa_w_putki(9615)
+set_yla_from_db(9613); set_maa_w_putki(9613) # p3
+set_yla_from_db(9615); set_maa_w_putki(9615) # p26
 
 # Kuuminainen 0204 p8 ja p8AM tietokannasta (DTM tukee hyvin, ELY hieman pielessä)
-set_yla_from_db(9739); set_maa_w_putki(9739)
-set_yla_w_id(86005, from = 9739); set_maa_w_id(86005, from = 9739)
+set_yla_from_db(9739); set_maa_w_putki(9739) # p8
+set_yla_w_id(86005, from = 9739); set_maa_w_id(86005, from = 9739) # p8AM
 
-# Orivesi 0301 p10b ja p10bAM yläpäät DTMstä+putket.
-set_yla_from_dtm(43837); set_maa_w_putki(43837) # p10b
-set_yla_w_id(59765, from = 43837); set_maa_w_id(59765, from = 43837) # p10bAM
+
 # Orivesi 0301 p10 yläpää DTM+putki, mutta putkesta 46cm. Maanpinta suoraan DTM.
 putki_yhd_temp[putki_yhd_temp$paikka_id == 9673, "putki"] <- 0.46
 putki_yhd_temp[putki_yhd_temp$paikka_id == 9673, "putki_source"] <- "man. chg."
 set_yla_from_dtm(9673) # DTM + yllä asetettu putken pituus 46cm
 set_maa_w_putki(9673) # (DTM + 46cm) - 46cm
-# Orivesi p3, p3b, p3bAM DTM+putket yläpäiksi. Maanpinta yläpäiden mukaan.
+# Orivesi 0301 p10b ja p10bAM, p3, p3b, p3bAM yläpäät DTMstä+putket.
+set_yla_from_dtm(43837); set_maa_w_putki(43837) # p10b
+set_yla_w_id(59765, from = 43837); set_maa_w_id(59765, from = 43837) # p10bAM
 set_yla_from_dtm(9666); set_maa_w_putki(9666) # p3
 set_yla_from_dtm(43831); set_maa_w_putki(43831) # p3b
 set_yla_w_id(59764, from = 43831); set_maa_w_id(59764, from = 43831) # p3bAM
+
 
 # Jämijärvi 0302 p9/p9AM, p4, p5 yläpäät tietokannasta. Maanpinnat putkien mukaan.
 set_yla_from_db(9848); set_maa_w_putki(9848) # p9
 set_yla_w_id(65021, from = 9848); set_maa_w_id(65021, from = 9848) # p9AM
 set_yla_from_db(9845); set_maa_w_putki(9845) # p4
 set_yla_from_db(9842); set_maa_w_putki(9842) # p5
+
 
 # Siikainen 0303 p3, p9, p10, p10AM yläpää tietokannasta
 set_yla_from_db(9675); set_maa_w_putki(9675) # p3
@@ -126,6 +128,7 @@ set_yla_from_dtm(73607); set_maa_w_putki(73607) # p4b
 set_yla_from_dtm(73608); set_maa_w_putki(73608) # p5b
 set_yla_from_dtm(73609); set_maa_w_putki(73609) # p7b
 set_yla_from_dtm(73610); set_maa_w_putki(73610) # p10b
+
 
 # Seitseminen 0306 p2,p2AM, p3,p3AM, p4,p4AM, p5, p6,p6AM, p8,p8AM yläp db:stä
 set_yla_from_db(45943); set_maa_w_putki(45943) # p2
@@ -151,6 +154,7 @@ set_yla_from_km2(75346); set_maa_w_putki(75346) # limni
 set_yla_from_db(10097); set_maa_w_putki(10097) # p2
 set_yla_from_db(10099); set_maa_w_putki(10099) # p8
 
+
 # Pertunmaa 0501 p4 yläpää tietokannasta
 set_yla_from_db(10126); set_maa_w_putki(10126) # p4
 # Pertunmaa 0501 p7, p7AM, p9b yläpäät DTM:stä
@@ -158,14 +162,15 @@ set_yla_from_dtm(10132); set_maa_w_putki(10132) # p7
 set_yla_w_id(88330, from = 10132); set_maa_w_id(88330, from = 10132) # p7AM
 set_yla_from_dtm(45779); set_maa_w_putki(45779) # p9b
 
-# Kangaslahti Rautavaara 0603 p6b,p7b yläp vanh putkista miinus b- ja ei-b-putkien vanh ero (jotka valokuv mukaiset)
+
+# Kangaslahti Rautavaara 0603 p6b, p7b/p7bAM yläp vanh putkista miinus b- ja ei-b-putkien vanh ero (jotka valokuv mukaiset)
 putki_yhd_temp[putki_yhd_temp$paikka_id == 41917, "yla"] <- #p6b:n yläksi...
-  putki_yhd_temp[putki_yhd_temp$paikka_id == 10018, "yla"] - #p6:n val.ylä, miin
+  putki_yhd_temp[putki_yhd_temp$paikka_id == 10018, "yla"] - #p6:n valittu ylä, miin
   (putki_yhd_temp[putki_yhd_temp$paikka_id == 10018, "yla_db"] - #6sten vanh ero
      putki_yhd_temp[putki_yhd_temp$paikka_id == 41917, "yla_db"])
 set_maa_w_putki(41917)
 putki_yhd_temp[putki_yhd_temp$paikka_id == 41918, "yla"] <- #p7b:n yläksi...
-  putki_yhd_temp[putki_yhd_temp$paikka_id == 10019, "yla"] - #p7:n val.ylä, miin
+  putki_yhd_temp[putki_yhd_temp$paikka_id == 10019, "yla"] - #p7:n valittu ylä, miin
   (putki_yhd_temp[putki_yhd_temp$paikka_id == 10019, "yla_db"] - #7jen vanh ero
      putki_yhd_temp[putki_yhd_temp$paikka_id == 41918, "yla_db"])
 set_maa_w_putki(41918)
@@ -175,6 +180,7 @@ set_maa_w_id(41920, from = 10021) # Aseta aluksi p9b maa p9:n mukaan
 putki_yhd_temp[putki_yhd_temp$paikka_id == 41920, "yla"] <- #p9b ylä putk pit muk
   putki_yhd_temp[putki_yhd_temp$paikka_id == 41920, "maa"] +
   putki_yhd_temp[putki_yhd_temp$paikka_id == 41920, "putki"]
+
 
 # Kuuksenvaara Ilomantsi 0701 p4 ja p4b yläpää DTM:stä
 set_yla_from_dtm(10557); set_maa_w_putki(10557) # p4
@@ -190,8 +196,10 @@ set_maa_w_putki(10563)
 # Kuuksenvaara Ilomantsi 0701 p3b yläpää KM2:sta
 set_yla_from_km2(41039); set_maa_w_putki(41039)
 
+
 # Jaamankangas 0702 p2b yläpää KM2:sta
 set_yla_from_km2(40881); set_maa_w_putki(40881)
+
 
 # Jakokoski Kontiolahti 0703 p7 ja p10 yläpää tietokannasta
 set_yla_from_db(10081); set_maa_w_putki(10081) # p7
@@ -202,10 +210,10 @@ putki_yhd_temp[putki_yhd_temp$paikka_id == 10076, "yla"] <-
   putki_yhd_temp[putki_yhd_temp$paikka_id == 10076, "putki"]
 set_maa_w_putki(10076)
 
+
 # Hietajärvi Patvinsuo 0705 p1AM yläp tietokannasta, p1 identtiseksi p1AM kanssa
 set_yla_from_db(63979); set_maa_w_putki(63979) # p1AM
 set_yla_w_id(14363, from = 63979); set_maa_w_id(14363, from = 63979) # p1
-
 
 # Rajamäki Karijoki 0801 p11b yläpää tietokannasta
 set_yla_from_db(42048); set_maa_w_putki(42048) # p11b
@@ -269,7 +277,7 @@ set_yla_w_id(88354, from = 9655); set_maa_w_id(88354, from = 9655) # p9AM
 set_yla_from_db(9656);  set_maa_w_putki(9656)  # p10
 
 # Vehkoo Multia 0902 p6b ja p6bAM yläpää putkikortista (163,09)
-putki_yhd_temp[putki_yhd_temp$paikka_id == 43289, "yla"] <- 163.09
+putki_yhd_temp[putki_yhd_temp$paikka_id == 43289, "yla"] <- 163.09 # p6b
 set_maa_w_putki(43289)
 set_yla_w_id(88295, from = 43289); set_maa_w_id(88295, from = 43289) # p6bAM
 
@@ -291,7 +299,6 @@ set_yla_from_db(10007); set_maa_w_putki(10007) # p13
 set_yla_from_db(44421); set_maa_w_putki(44421) # p10b
 set_yla_w_id(82236, from = 44421); set_maa_w_id(82236, from = 44421) # p10bAM
 
-
 # Kälviä 1003 p6, p10 yläpää tietokannasta
 set_yla_from_db(9746); set_maa_w_putki(9746) # p6
 set_yla_from_db(9750); set_maa_w_putki(9750) # p10
@@ -306,11 +313,11 @@ set_maa_w_putki(9785)
 
 
 # Lumiaho 1202 p1b yläpää DTM:stä
-set_yla_from_db(42113); set_maa_w_putki(42113)
+set_yla_from_dtm(42113); set_maa_w_putki(42113)
 # Lumiaho 1202limni,limAM putket 106 cm kuten p11:ssa, p11 koord korjattiin limnin koordksi, jolloin yläp tulee samaksi kuin limnillä/limAM:llä
 putki_yhd_temp[putki_yhd_temp$paikka_id == 37921, "putki"] <- 1.06 
 putki_yhd_temp[putki_yhd_temp$paikka_id == 37921, "putki_source"] <- "DB(1202p11)"
-set_yla_from_dtm(37921); set_maa_w_putki(37921) # 1202limni
+set_yla_from_dtm(37921); set_maa_w_putki(37921) # 1202limni (DTM was selected by def)
 set_yla_w_id(64028, from = 37921); set_maa_w_id(64028, from = 37921) #1202limAM
 set_yla_w_id(9961, from = 37921); set_maa_w_id(9961, from = 37921) #p11 (just in case)
 
@@ -318,6 +325,7 @@ set_yla_w_id(9961, from = 37921); set_maa_w_id(9961, from = 37921) #p11 (just in
 # Alakangas 1203 p10 ja p10b yläpäät tietokannasta
 set_yla_from_db(9570); set_maa_w_putki(9570) # p10
 set_yla_from_db(43427); set_maa_w_putki(43427) # p10b
+
 
 # Kullisuo 1204 p11 yläpää limnin yläpään mukaiseksi + koord korjattu vastaaviksi
 set_yla_w_id(9959, from = 37922); set_maa_w_putki(9959) # p11
@@ -328,12 +336,18 @@ putki_yhd_temp[putki_yhd_temp$paikka_id == 82270, "putki"] <- #limAM
   putki_yhd_temp[putki_yhd_temp$paikka_id == 9959, "putki"]
 putki_yhd_temp[putki_yhd_temp$paikka_id == 37922, "putki_source"] <- "DB(1204p11)"
 putki_yhd_temp[putki_yhd_temp$paikka_id == 82270, "putki_source"] <- "DB(1204p11)"
-set_maa_w_putki(37922); set_maa_w_putki(82270)
-
-# Jokiniemi Pesiö 1209 p1,p1AM, p10, limni,limAM yläp 2000-luvun vaait mukaan
+set_maa_w_putki(37922); set_maa_w_putki(82270) # limni/limAM ground
 
 
-# Jokiniemi Pesiö 1209 p2,p2AM, p4, p8,p8AM, p10 yläpäät tietokannasta
+# Jokiniemi Pesiö 1209p1/p1AM, p10/limni/limAM yläp 2000-luv vaait mukaan (p10=limni)
+putki_yhd_temp[putki_yhd_temp$paikka_id == 9945, "yla"] <- 218.7677 # p1
+set_maa_w_putki(9945)
+set_yla_w_id(86020, from = 9945); set_maa_w_id(86020, from = 9945)  # p1AM
+putki_yhd_temp[putki_yhd_temp$paikka_id == 9944, "yla"] <- 219.3402 # p10
+set_maa_w_putki(9944)
+set_yla_w_id(37891, from = 9944); set_maa_w_id(37891, from = 9944) # limni
+set_yla_w_id(86023, from = 9944); set_maa_w_id(86023, from = 9944) # limAM 
+# Jokiniemi Pesiö 1209 p2/p2AM, p4, p8/p8AM, p10 yläpäät tietokannasta
 set_yla_from_db(9939); set_maa_w_putki(9939) # p2
 set_yla_w_id(86021, from = 9939); set_maa_w_id(86021, from = 9939) # p2AM
 set_yla_from_db(9940); set_maa_w_putki(9940) # p4
@@ -342,7 +356,7 @@ set_yla_w_id(86022, from = 9942); set_maa_w_id(86022, from = 9942) # p8AM
 set_yla_from_db(9944); set_maa_w_putki(9944) # p10
 
 
-# Könölä Tornio 1301p3, p4,limni,p5/p5b/p5bAM,p7/p7b/p7bAM,p8/p8AM yläpää db:stä
+# Könölä Tornio 1301 p3, p4,limni,p5/p5b/p5bAM,p7/p7b/p7bAM,p8/p8AM yläpää db:stä
 set_yla_from_db(9810); set_maa_w_putki(9810) # p3
 set_yla_from_db(9716); set_maa_w_putki(9716) # p4
 set_yla_from_db(78408); set_maa_w_putki(78408) # limni
